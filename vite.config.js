f8023454e25913f5949c1bc7614110db17ebf908
Web-Teams <webteam291@gmail.com>
2025-04-23 14:25:04 +0200

    add flowbite

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
+import flowbiteReact from "flowbite-react/plugin/vite";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // __dirname is not defined in ES modules
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), tailwindcss(), flowbiteReact()],
 });
