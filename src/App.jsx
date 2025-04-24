9f3500861eedf3f4c6fc6f00f66726e6b9034a63
Ahmed Zakaria <ahmedzakaria10az@gmail.com>
2025-04-24 22:47:07 +0200

    add BestChefs.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import MainMeals from "./components/custom/HomePageComponents/MainMeals";
 import WhyUsComponents from "./components/custom/HomePageComponents/WhyUsComponents";
 import ChifsRating from "./components/custom/HomePageComponents/ChifsRating";
 import HowItIsWorkComponent from "./components/custom/HomePageComponents/HowItIsWorkComponent";
-
+import BestChefs from "./components/custom/HomePageComponents/BestChefs";
 function App() {
   return (
     <>
-  {/* <MainMeals/>
-
+ <MainMeals/>
+ <br />
+ <BestChefs/> 
+<br />
   <WhyUsComponents/>
-
    <ChifsRating/>
-
-   <HowItIsWorkComponent/> */}
+   <HowItIsWorkComponent/> 
    </>
      
   );
